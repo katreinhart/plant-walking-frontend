318b76b473a0ba914325944802da1f0319fe4245
RJ <jensenrs@gmail.com>
2017-12-19 10:45:00 -0800

    styling links and hover on home view

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ class App extends Component {
     return (
       <div>
         <Navigation />
-        <div className ="plant-container">
-          <div className ="carousel-arrow left">
-            <i className ="material-icons">keyboard_arrow_left</i>
+        <div className="home-container">
+          <div className ="plant-container">
+            <div className ="carousel-arrow left">
+              <i className ="material-icons">keyboard_arrow_left</i>
+            </div>
+              <img className ="plant-image" alt="seedling" src="./images/seedling_plant.png" />
+            <div className ="carousel-arrow right">
+              <i className ="material-icons">keyboard_arrow_right</i>
+            </div>
           </div>
-            <img className ="plant-image" alt="seedling" src="./images/seedling_plant.png" />
-          <div className ="carousel-arrow right">
-            <i className ="material-icons">keyboard_arrow_right</i>
+          <div className ="progress-bar">
           </div>
-        </div>
-        <div className ="progress-bar">
-        </div>
-        <div className="add-steps-container">
-          <input className="add-steps-input" type="text" />
-          <button className="add-steps-button">Add Steps</button>
-        </div>
-        <div className="view-garden-container">
-          <a href="#">View Garden</a>
-          <i className ="material-icons">keyboard_arrow_down</i>
+          <div className="add-steps-container">
+            <input className="add-steps-input" type="text" />
+            <button className="add-steps-button">Add Steps</button>
+          </div>
+          <div className="view-garden-container">
+            <a href="#">View Garden</a>
+            <i className ="material-icons">keyboard_arrow_down</i>
+          </div>
         </div>
       </div>
     );

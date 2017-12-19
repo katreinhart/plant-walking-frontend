6402c7cc845ab94a2868ff642b918e993472a2c7
RJ <jensenrs@gmail.com>
2017-12-18 17:31:34 -0800

    some front page styles added, blocking in layout

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <div className ="plant-container">
+          <div className ="carousel-arrow left">
+            <i className ="material-icons">keyboard_arrow_left</i>
+          </div>
+            <img className ="plant-image" alt="seedling" src="./images/seedling_plant.png" />
+          <div className ="carousel-arrow right">
+            <i className ="material-icons">keyboard_arrow_right</i>
+          </div>
+        </div>
+        <div className ="progress-bar">
+        </div>
+        <div className="add-steps-container">
+          <input className="add-steps-input" type="text" />
+          <button className="add-steps-button">Add Steps</button>
+        </div>
+        <div className="view-garden-container">
+          <a href="#">View Garden</a>
+          <i className ="material-icons">keyboard_arrow_down</i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav>
+      <div>brand</div>
+      <i className="material-icons">menu</i>
+    </nav>
+  )
+}
+
+export default Navigation

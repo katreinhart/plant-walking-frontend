57ca52348adaaffc7aab74f5737788747e25b4f3
Kat Reinhart <kat.reinhart@gmail.com>
2017-12-20 13:28:48 -0800

    Start wiring up home components

--- src/App.js
+++ src/App.js
@@ -9,13 +9,22 @@ import PastPlantDetails from './components/PastPlantDetails'
 import CloseForm from './components/CloseForm'
 
 class App extends Component {
+  addSteps (e) {
+    e.preventDefault()
+    const input = e.target.querySelector('.add-steps-input')
+    const stepsAdded = input.value
+    input.value = ''
+    console.log(`You just added ${stepsAdded} steps`)
+  }
+
   render() {
     return (
       <div className="outermost-container">
         <Navigation />
-          <CloseForm />
-          <PlantContainer />
-            <PastPlantDetails />
+        <PlantContainer />
+        <ProgressBar />
+        <AddStepsContainer addSteps={ this.addSteps } />
+        <ViewGarden />
       </div>
     );
   }

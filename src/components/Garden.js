4fc10eddda99d1d0f75a63c46c067d3b25c34d2a
Kat Reinhart <kat.reinhart@gmail.com>
2017-12-20 14:11:33 -0800

    Move grid stuff into Garden component

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,11 +1,79 @@
 import React, { Component } from 'react'
+import Navigation from './Navigation'
 
 class Garden extends Component {
   state = {}
   render() { 
     return ( 
-      <div>
-        Your beautiful garden here
+      <div className="outermost-container">
+        <Navigation />
+          <div className="back-to-plant-container">
+            <i className="material-icons">keyboard_arrow_up</i>
+            <a href="#">Back to plant</a>
+          </div>
+
+          <div className="garden-container">
+
+            <div className="garden-row">
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+            </div>
+
+            <div className="garden-row">
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+            </div>
+
+            <div className="garden-row">
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box odd-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+            </div>
+
+            <div className="garden-row">
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+              <div className="garden-plant-box even-plant-row">
+                <i className=" plant material-icons">insert_emoticon</i>
+              </div>
+            </div>
+          </div>
       </div>
      )
   }

78b76ab2d099ee7c34d0cb1dd4fd6652f9896ea1
RJ <jensenrs@gmail.com>
2017-12-20 16:54:36 -0800

    close form includes hr tag

--- src/components/CloseForm.js
+++ src/components/CloseForm.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const CloseForm = ({ title }) => {
   return (
-    <div className="close-form">
-      <h3 className="title">{ title }</h3>
-      <i className="material-icons close-x">close</i>
+    <div>
+      <div className="close-form">
+        <h3 className="title">{ title }</h3>
+        <i className="material-icons close-x">close</i>
+      </div>
+      <hr></hr>
     </div>
   )
 }

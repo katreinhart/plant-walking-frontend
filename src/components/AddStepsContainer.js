34f6f78e6ca2699aa7bda8c71f1b0d9bd28d5c97
RJ <jensenrs@gmail.com>
2017-12-21 11:29:17 -0800

    edit steps view added

--- src/components/AddStepsContainer.js
+++ src/components/AddStepsContainer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const AddStepsContainer = ({ addSteps }) => {
   return (
-    <form className="add-steps-container" onSubmit={ addSteps }>
-      <input className="add-steps-input" type="text" />
-      <button className="add-steps-button">Add Steps</button>
+    <form className="input-container" onSubmit={ addSteps }>
+      <input className="input-field" type="text" />
+      <button className="button add-steps-button">Add Steps</button>
     </form>
   )
 }

383f5e43a5bc6fd92d95a567a8e4a2ffb5d9dbdc
RJ <jensenrs@gmail.com>
2017-12-29 12:42:01 -0800

    separated css into mini files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
+import './css/general.css';
+import './css/home.css';
+import './css/garden.css';
+import './css/pastplant.css';
+import './css/forms.css';
+import './css/history.css';
+import './css/welcome.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 

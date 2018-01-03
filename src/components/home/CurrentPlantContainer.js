eec847d62cccc6e6ead32a2d6e37a2e95968d315
RJ <jensenrs@gmail.com>
2018-01-03 15:46:33 -0800

    tugging on plantId data for current plant iamge

--- src/components/home/CurrentPlantContainer.js
+++ src/components/home/CurrentPlantContainer.js
@@ -11,8 +11,22 @@ class CurrentPlantContainer extends React.Component {
   }
 
   render () {
-    return <div>{ JSON.stringify(this.props.currentPlant) }</div>
+    return (
+      <div className ="plant-container">
+        <img className ="plant-image" alt="current plant" src="./images/seedling_plant.png" />
+      </div>
+    )
   }
 }
 
 export default CurrentPlantContainer
+
+// stepsRecorded -> 50
+// endSteps -> 100
+// 50 / 100
+// frames -> 40
+// 40 * (50 / 100)
+
+// <div>{ JSON.stringify(this.props.currentPlantId) }</div>
+//this.props.currentPlantId
+//this.props.stepsRecorded

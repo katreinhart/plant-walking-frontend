15cdbe0ca3abefe5dcbc3c35b5d48855a48944ae
Sean Obringer <SeanObringer90@gmail.com>
2018-01-03 14:53:40 -0800

    get garden updated

--- src/components/garden/Garden.js
+++ src/components/garden/Garden.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import React, { Component } from 'react'
+import GardenPlant from './GardenPlant.js'
 import { Link } from 'react-router-dom'
 import Navigation from '../shared/Navigation'
 import BackToPlant from './BackToPlant'
+
 const localhostURL = 'http://localhost:2999/api'
 
 class Garden extends Component {
@@ -32,9 +34,10 @@ class Garden extends Component {
 
           <div className="garden-container mt-5">
             <div className="garden-row">
-              <div className="garden-plant-box odd-plant-row">
-                <i className=" plant material-icons">insert_emoticon</i>
-              </div>
+              {/* <div className="garden-plant-box odd-plant-row">
+                <img src='./images/plant-1-completed.png' width='50%'/>
+              </div> */}
+              <GardenPlant/>
               <div className="garden-plant-box odd-plant-row">
                 <i className=" plant material-icons">insert_emoticon</i>
               </div>
---
+++ src/components/garden/GardenPlant.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+// import { Link } from 'react-router-dom'
+
+const GardenPlant =() => {
+
+  return (
+    <div className="garden-plant-box odd-plant-row">
+      <img src='./images/plant-1-completed.png' width='50%'/>
+    </div>
+  )
+
+}
+
+
+export default  GardenPlant
